a9dce05e42940693ae9268c41c123005892e3510
JohnSmithW <salapin.danil1@mail.ru>
2025-02-16 00:33:02 +0200

    fix(ui): test fix
    Fixed test for home page

--- src/pages/home/__tests__/home-page.test.tsx
+++ src/pages/home/__tests__/home-page.test.tsx
@@ -1,11 +1,16 @@
 import '@testing-library/jest-dom';
 import { describe, it, expect } from 'vitest';
+import { MemoryRouter } from 'react-router-dom';
 import { render, screen } from '@testing-library/react';
 import { HomePage } from '../ui';
 
 describe('CharacterForm', () => {
   it('should render', async () => {
-    render(<HomePage />);
+    render(
+      <MemoryRouter>
+        <HomePage />
+      </MemoryRouter>
+    );
 
     expect(screen.getByTestId('home-page')).toBeInTheDocument();
   });
